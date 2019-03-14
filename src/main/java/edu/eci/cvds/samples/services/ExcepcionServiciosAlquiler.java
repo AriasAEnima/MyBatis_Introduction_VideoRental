ddf1276e741b242511f766bd4937ecf6a6c45a08
Eduardo Arias <julianeduardoarias@outlook.com>
2019-03-14 08:25:37 -0500

    constructores excepciones

--- src/main/java/edu/eci/cvds/samples/services/ExcepcionServiciosAlquiler.java
+++ src/main/java/edu/eci/cvds/samples/services/ExcepcionServiciosAlquiler.java
@@ -5,10 +5,20 @@
  */
 package edu.eci.cvds.samples.services;
 
+import edu.eci.cvds.sampleprj.dao.PersistenceException;
+
 /**
  *
  * @author 2131381
  */
 public class ExcepcionServiciosAlquiler extends Exception {
+
+    public ExcepcionServiciosAlquiler(String string, PersistenceException ex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ExcepcionServiciosAlquiler(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
